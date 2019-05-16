62b104f0ea4c6c70edfc61e05b463da700b470f4
lazyKEV <viveksinha.301@gmail.com>
2019-05-16 18:35:10 +0530

    image upload using base64

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import base64
 import os
 from io import BytesIO
 
 from dotenv import load_dotenv
+from secrets import token_hex
 from flask import (Flask, Response, flash, redirect, render_template, request,
                    session, url_for)
 from flask_bootstrap import Bootstrap
@@ -9,7 +11,7 @@ from PIL import Image
 
 from filters import datetimeformat, file_type
 from resources import (_get_s3_client, get_bucket, get_buckets_list,
-                       get_presigned_url)
+                       get_presigned_url, _get_s3_resource)
 
 load_dotenv('.env')
 
@@ -61,6 +63,25 @@ def upload():
 
     flash('File uploaded successfully')
     return redirect(url_for('files'))
+
+
+@app.route('/upload_b64', methods=["GET", "POST"])
+def upload_b64():
+    if request.method == "POST":
+        try:
+            img = base64.b64decode(request.form['img_string'])
+            buf = BytesIO(img)
+        except Exception as err:
+            print("[ERROR]: " + str(err))
+            return redirect(url_for('upload_b64'))
+
+        s3 = _get_s3_resource()
+        my_bucket = s3.Bucket("azsxcx")
+        image_path = f"images/" + token_hex(8) + ".jpg"
+        my_bucket.Object(image_path).put(Body=buf.getvalue())    # noqa
+        return redirect(url_for('index'))
+
+    return render_template('upload_b64.html')
 
 
 @app.route('/delete', methods=['POST'])
